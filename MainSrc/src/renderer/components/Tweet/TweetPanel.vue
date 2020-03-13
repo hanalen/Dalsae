999e04d236c3d2829ae0596bd70d50843156fb28
hanalen <jinsub2001@naver.com>
2020-03-13 17:35:11 +0900

    스페이스

--- MainSrc/src/renderer/components/Tweet/TweetPanel.vue
+++ MainSrc/src/renderer/components/Tweet/TweetPanel.vue
@@ -68,96 +68,96 @@ export default {
   },
   mounted: function() {//EventBus등록용 함수들
     this.EventBus.$on('TweetKeyDown', (e) => {
-        if(e.keyCode==49){
-          this.$store.dispatch('ClearDaehwa');
-          this.selectPanelName='home';
-          this.prevPanelName='home';
-          this.selectPanel.Focus(e);
-        }
-        else if(e.keyCode==50){
-          this.$store.dispatch('ClearDaehwa');
-          this.selectPanelName='mention';
-          this.prevPanelName='mention';
-          this.selectPanel.Focus(e);
-        }
-        else if(e.keyCode==36){
-          this.selectPanel.Home(e);
-        }
-        else if(e.keyCode==35){
-          this.selectPanel.End(e);
-        }
-        else if(e.keyCode==32){
-          e.preventDefault();
-          this.ReqTweets(this.selectPanelName);
-        }
-        else if(e.keyCode==99|| e.keyCode==67){//c, 대화 트윗 불러오기
-          e.preventDefault();
-          var tweet =this.selectPanel.GetSelectTweet();
-          if(!tweet.orgTweet.in_reply_to_status_id_str) return;//대화 없는건 스킵 
-          this.$store.dispatch('DaehwaAutoAdd', tweet);//캐시데이터가 있는지부터 체크 
+      if(e.keyCode==49){
+        this.$store.dispatch('ClearDaehwa');
+        this.selectPanelName='home';
+        this.prevPanelName='home';
+        this.selectPanel.Focus(e);
+      }
+      else if(e.keyCode==50){
+        this.$store.dispatch('ClearDaehwa');
+        this.selectPanelName='mention';
+        this.prevPanelName='mention';
+        this.selectPanel.Focus(e);
+      }
+      else if(e.keyCode==36){
+        this.selectPanel.Home(e);
+      }
+      else if(e.keyCode==35){
+        this.selectPanel.End(e);
+      }
+      else if(e.keyCode==32){
+        e.preventDefault();
+        this.ReqTweets(this.selectPanelName);
+      }
+      else if(e.keyCode==99|| e.keyCode==67){//c, 대화 트윗 불러오기
+        e.preventDefault();
+        var tweet =this.selectPanel.GetSelectTweet();
+        if(!tweet.orgTweet.in_reply_to_status_id_str) return;//대화 없는건 스킵 
+        this.$store.dispatch('DaehwaAutoAdd', tweet);//캐시데이터가 있는지부터 체크 
 
-          if(this.$store.state.tweets.daehwa[0].orgTweet.in_reply_to_status_id_str){//캐시 정리가 끝난 후 대화가 있을 경우에 api 콜
-            this.EventBus.$emit('LoadingTweetPanel', {'isLoading': true, 'panelName':'daehwa'})
-            this.EventBus.$emit('ReqDaehwa', tweet);
-          }
-          else{
-            this.EventBus.$emit('LoadingTweetPanel', {'isLoading': false, 'panelName':'daehwa'})//로딩 뱅글이 끄기위해 호출
-            this.EventBus.$emit('FocusPanel', 'daehwa');//패널 변경
-          }
+        if(this.$store.state.tweets.daehwa[0].orgTweet.in_reply_to_status_id_str){//캐시 정리가 끝난 후 대화가 있을 경우에 api 콜
+          this.EventBus.$emit('LoadingTweetPanel', {'isLoading': true, 'panelName':'daehwa'})
+          this.EventBus.$emit('ReqDaehwa', tweet);
         }
-        else if(e.keyCode==8){//backspace, 대화 숨기기. vuex의 대화 목록을 clear
-          e.preventDefault();
-          this.EventBus.$emit('FocusPanel', this.prevPanelName);
+        else{
+          this.EventBus.$emit('LoadingTweetPanel', {'isLoading': false, 'panelName':'daehwa'})//로딩 뱅글이 끄기위해 호출
+          this.EventBus.$emit('FocusPanel', 'daehwa');//패널 변경
         }
-        else if(e.keyCode==88||e.keyCode==120){//x, qt트윗 등록 
-          e.preventDefault();
-          var tweet= this.selectPanel.GetQTTweet();
-          if(tweet){
-            this.$store.dispatch('Daehwa', tweet);
-            this.EventBus.$emit('FocusPanel', 'daehwa');//패널 변경
-          }
+      }
+      else if(e.keyCode==8){//backspace, 대화 숨기기. vuex의 대화 목록을 clear
+        e.preventDefault();
+        this.EventBus.$emit('FocusPanel', this.prevPanelName);
+      }
+      else if(e.keyCode==88||e.keyCode==120){//x, qt트윗 등록 
+        e.preventDefault();
+        var tweet= this.selectPanel.GetQTTweet();
+        if(tweet){
+          this.$store.dispatch('Daehwa', tweet);
+          this.EventBus.$emit('FocusPanel', 'daehwa');//패널 변경
         }
-        else if(e.keyCode==65 || e.keyCode == 97){//a,전체 답변
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          this.EventBus.$emit('ReplyAll', tweet);
-        }
-        else if(e.keyCode==82 || e.keyCode==114){//r, 답변
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          this.EventBus.$emit('Reply', tweet);
-        }
-        else if(e.keyCode==70||e.keyCode==102){//f, 관글
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          this.EventBus.$emit('Favorite', tweet);
-        }
-        else if(e.keyCode==84||e.keyCode==116){//t, 리트윗
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          this.EventBus.$emit('Retweet', tweet);
-        }
-        else if(e.keyCode==71||e.keyCode==103){//g, 이미지열기
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          var ipcRenderer = require('electron').ipcRenderer;
-          ipcRenderer.send('child', tweet, this.$store.state.DalsaeOptions.uiOptions);
-        }
-        else if(e.keyCode==86||e.keyCode==118){//v, 컨텍스트 메뉴
-          e.preventDefault();
-          this.selectPanel.ShowContextMenu();
-        }
-        else if(e.keyCode==85||e.keyCode==117){//u, 인풋 포커스
-          e.preventDefault();
-          this.EventBus.$emit('FocusInput');
-        }
-        else if(e.keyCode==72||e.keyCode==104){//h, 해시태그, 해시 미구현
-        }
-        else if(e.keyCode==127|| e.keyCode==46){//del, 트윗 삭제
-          e.preventDefault();
-          var tweet=this.selectPanel.GetSelectTweet();
-          this.EventBus.$emit('DeleteTweet', tweet);
-        }
+      }
+      else if(e.keyCode==65 || e.keyCode == 97){//a,전체 답변
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        this.EventBus.$emit('ReplyAll', tweet);
+      }
+      else if(e.keyCode==82 || e.keyCode==114){//r, 답변
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        this.EventBus.$emit('Reply', tweet);
+      }
+      else if(e.keyCode==70||e.keyCode==102){//f, 관글
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        this.EventBus.$emit('Favorite', tweet);
+      }
+      else if(e.keyCode==84||e.keyCode==116){//t, 리트윗
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        this.EventBus.$emit('Retweet', tweet);
+      }
+      else if(e.keyCode==71||e.keyCode==103){//g, 이미지열기
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        var ipcRenderer = require('electron').ipcRenderer;
+        ipcRenderer.send('child', tweet, this.$store.state.DalsaeOptions.uiOptions);
+      }
+      else if(e.keyCode==86||e.keyCode==118){//v, 컨텍스트 메뉴
+        e.preventDefault();
+        this.selectPanel.ShowContextMenu();
+      }
+      else if(e.keyCode==85||e.keyCode==117){//u, 인풋 포커스
+        e.preventDefault();
+        this.EventBus.$emit('FocusInput');
+      }
+      else if(e.keyCode==72||e.keyCode==104){//h, 해시태그, 해시 미구현
+      }
+      else if(e.keyCode==127|| e.keyCode==46){//del, 트윗 삭제
+        e.preventDefault();
+        var tweet=this.selectPanel.GetSelectTweet();
+        this.EventBus.$emit('DeleteTweet', tweet);
+      }
     });
 		this.EventBus.$on('focusTweet', (e) => {
       this.selectPanel.Focus(e);
