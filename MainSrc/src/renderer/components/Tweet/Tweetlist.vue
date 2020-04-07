17a32bec10a8372635eb0e41a525331d0ba01815
hanalen <jinsub2001@naver.com>
2020-04-07 18:12:59 +0900

    홈/엔드키 이동 추가

--- MainSrc/src/renderer/components/Tweet/Tweetlist.vue
+++ MainSrc/src/renderer/components/Tweet/Tweetlist.vue
@@ -181,12 +181,14 @@ export default {
       }
     },
     End(e){
+      this.ScrollToTweet(this.tweets.length-1)
       return;//홈엔드 로직에 문제 있어 일단 막음
       if(this.tweets===undefined) return;
       this.selectIndex=this.tweets.length-1;
       this.FocusTweet(e);
     },
     Home(e){
+      this.ScrollToTweet(0);
       return;//홈엔드 로직에 문제 있어 일단 막음
       if(this.tweets===undefined) return;
       this.selectIndex=0;
