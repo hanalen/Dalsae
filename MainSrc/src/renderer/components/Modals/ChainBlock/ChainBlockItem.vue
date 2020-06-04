f52c67da07df876923bde828d8defeb202519f8d
hanalen <jinsub2001@naver.com>
2020-06-04 22:07:34 +0900

    체인블락 html 수정 및 에러 처리

--- MainSrc/src/renderer/components/Modals/ChainBlock/ChainBlockItem.vue
+++ MainSrc/src/renderer/components/Modals/ChainBlock/ChainBlockItem.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="chain-block-item">
-		<span>{{user.screen_name}}의 {{strFollow}}를 전부 차단 합니다.</span>
-		<span>{{status}}</span><br/>
-		<span>{{blockCount}} / {{maxBlockCount}}</span><br/>
-		<ProgressBar ref="progress" />
-		<select size='6'>
-			<option v-for="(item, index) in listSkip" :key="index">{{item}}</option>
-		</select>
-		<input type="button" value="시작하기" @click="Start"/>
+		<span>현재 상태: {{status}}</span><br/>
+		<span v-if="!isFollowingList">{{user.screen_name}}의 팔로워를 전부 차단 합니다.</span>
+		<span v-if="isFollowingList">{{user.screen_name}}의 팔로잉을 전부 차단 합니다.</span>
+		<div class="progress-area" v-if="status != '대기 중'">
+			<span>진행 상황: </span>
+			<span>{{blockCount}} / {{maxBlockCount}}</span><br/>
+			<ProgressBar ref="progress" />
+			<span>차단 제외 목록</span>
+			<select size='6'>
+				<option v-for="(item, index) in listSkip" :key="index">{{item}}</option>
+			</select>
+		</div>
+		<div class="select-area" v-if="status=='대기 중'">
+			<input type="radio" id="radioFollowing" v-model="isFollowingList" :value="true">
+			<label for="radioFollowing">팔로잉을 차단</label>
+			<input type="radio" id="radioFollower" v-model="isFollowingList" :value="false"/>
+			<label for="radioFollower">팔로워를 차단</label>
+			<b-button class="btn" variant="primary" @click="Start">시작하기</b-button>
+		</div>
   </div>
 </template>
 
@@ -22,13 +33,10 @@ export default {
 			status:'대기 중',
 			listSkip:[],
 			blockCount:0,
+			isFollowingList:false,
 		}
 	},
 	props:{
-		isFollowingList:{
-			type:Boolean,
-			default:false,
-		},
 		userInfo:undefined,
 		user:undefined,
 		listFollowing:undefined,
@@ -64,6 +72,8 @@ export default {
 					console.log(item)
 				}
 			})
+			this.CheckFriends();
+			return;
 			if(res.next_cursor==0){
 				this.CheckFriends();
 			}
@@ -71,21 +81,38 @@ export default {
 				this.ReqList(res.next_cursor);
 			}
 		},
-		ResErr(err){
+		ResErr(err, cursor){
 			console.log('err req list')
-			console.log(err)
-			this.status='좌표 불러오기 실패';
+			const error=err.response.data.errors
+			console.log(err.response);
+			if(error){
+				if(error.length>0){
+					if(error[0].code==88){
+						this.status='팔로워 불러오기 리밋! 최대 15분 대기';
+      			setTimeout(() => this.ReqList(cursor), 60000);//리밋일 경우 1분 뒤 재요청
+					}
+					else{
+						this.status='뭔가 오류 개발자에게 문의 바람 code:'+error[0].code+ ' / message: '+error[0].message;
+					}
+				}
+				else{
+					this.status='알 수 없는 오류 개발자에게 문의 해주세요.'+JSON.stringify(error);
+				}
+			}
+			else{
+					this.status='알 수 없는 오류 개발자에게 문의 해주세요.'+JSON.stringify(error);
+			}
 		},
 		CheckFriends(){
 			this.listFollowing.forEach((user)=>{
 				if(this.hashUser.has(user.id_str)){
-					this.listSkip.push(user);
+					this.listSkip.push(user.screen_name+'/'+user.name);
 					this.hashUser.delete(user.id_str)
 				}
 			});
 			this.listFollower.forEach((user)=>{
 				if(this.hashUser.has(user.id_str)){
-					this.listSkip.push(user);
+					this.listSkip.push(user.screen_name+'/'+user.name);
 					this.hashUser.delete(user.id_str)
 				}
 			})
@@ -109,12 +136,6 @@ export default {
 		}
 	},
 	created:function(){
-		if(this.isFollowingList){
-			this.status=this.user.screen_name+'의 팔로잉 전체를 차단 합니다. 대기 중';
-		}
-		else{
-			this.status=this.user.screen_name+'의 팔로워 전체를 차단 합니다. 대기 중';
-		}
 	},
   mounted: function() {//EventBus등록용 함수들
 
@@ -125,5 +146,18 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-
+.btn {
+	font-size: 14px !important;
+	height: 30px;
+}
+.btn{
+	margin: 0px 4px;
+	width: 120px;
+	height: 22px;
+	font-size: 14px;
+	padding: 0;      
+	font-size: 14px !important;
+	padding: 0 6px !important;
+	height: 30px;
+}
 </style>
