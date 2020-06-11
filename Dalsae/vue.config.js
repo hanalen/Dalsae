9e6ef0362e1448170a485321c5b2028fc0c5ef98
hanalen <jinsub2001@naver.com>
2020-06-11 23:16:52 +0900

    vue config 수정

--- Dalsae/vue.config.js
+++ Dalsae/vue.config.js
@@ -1,4 +1,31 @@
 module.exports = {
+  pluginOptions: {
+    builderOptions: {},
+    electronBuilder: {
+      outputDir: 'electron-builder-output-dir',
+      chainWebpackMainProcess: config => {
+        // Chain webpack config for electron main process only
+      },
+      chainWebpackRendererProcess: config => {
+        // Chain webpack config for electron renderer process only
+        // The following example will set IS_ELECTRON to true in your app
+        config.plugin('define').tap(args => {
+          args[0]['IS_ELECTRON'] = true;
+          return args;
+        });
+      },
+      // Use this to change the entrypoint of your app's main process
+      mainProcessFile: 'src/background.ts'
+      // Provide an array of files that, when changed, will recompile the main process and restart Electron
+      // Your main process file will be added by default
+      // mainProcessWatch: ['src/myFile1', 'src/myFile2'],
+      // Provide a list of arguments that Electron will be launched with during "electron:serve",
+      // which can be accessed from the main process (src/background.js).
+      // Note that it is ignored when --debug flag is used with "electron:serve", as you must launch Electron yourself
+      // Command line args (excluding --debug, --dashboard, and --headless) are passed to Electron as well
+      // mainProcessArgs: ['--arg-name', 'arg-value']
+    }
+  },
   chainWebpack: config => {
     const svgRule = config.module.rule('svg');
 
