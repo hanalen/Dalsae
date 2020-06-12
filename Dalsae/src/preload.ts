449f3ff2318a016bf84dc088d57dacc5d02b9d15
hanalen <jinsub2001@naver.com>
2020-06-12 23:32:27 +0900

    기존 프리로드 삭제

--- Dalsae/src/preload.ts
+++ Dalsae/src/preload.ts
@@ -1,20 +1,21 @@
-import { remote } from 'electron';
+// import { remote } from 'electron';
 
-export default class Preload {
-  asdf() {
-    console.log('asdfasdf');
-    const v = new remote.BrowserWindow();
-    v.show();
-  }
-  asdf2() {
-    console.log('asdfasdf');
-  }
-  asdf3() {
-    console.log('asdfasdf');
-  }
-  asdf4() {
-    console.log('asdfasdf');
-  }
-}
+// export default class Preload {
+//   ClickLink() {
+//     console.log('asdfasdf');
+//     const v = new remote.BrowserWindow();
+//     v.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL as string}/test`);
+//     v.show();
+//   }
+//   asdf2() {
+//     console.log('asdfasdf');
+//   }
+//   asdf3() {
+//     console.log('asdfasdf');
+//   }
+//   asdf4() {
+//     console.log('asdfasdf');
+//   }
+// }
 
-window.preload = new Preload();
+// window.preload = new Preload();
