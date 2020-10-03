55829706c22958dc6396a85c605be4279a22c2c1
hanalen <jinsub2001@naver.com>
2020-10-04 03:29:31 +0900

    이미지 preload 연결

--- Dalsae/src/preloads/preload.ts
+++ Dalsae/src/preloads/preload.ts
@@ -1,10 +1,12 @@
-import { remote } from 'electron';
+import { BrowserWindow, remote } from 'electron';
 // export interface Preload {
 //   // asdf: () => void;
 //   asdf2: () => void;
 //   asdf3: () => void;
 //   asdf4: () => void;
 // }
+import Log from 'electron-log';
+import path from 'path';
 
 export default class Preload {
   // constructor() {
@@ -16,27 +18,32 @@ export default class Preload {
   //   if (!this._instence) {
   //   }
   // }
+  listWindow: BrowserWindow[] = [];
   ClickLink() {
     console.log('asdfasdf');
     const v = new remote.BrowserWindow({
-      show: true,
+      show: false,
       title: 'image-test',
       webPreferences: {
         // Use pluginOptions.nodeIntegration, leave this alone
         // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-        nodeIntegration: !!process.env.ELECTRON_NODE_INTEGRATION
+        nodeIntegration: !!process.env.ELECTRON_NODE_INTEGRATION,
+        preload: path.join(__dirname, 'preload')
       }
     });
     // router.push()
     // v.loadURL(URL.format());
-    v.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL as string}/test`);
+    console.log(`${process.env.WEBPACK_DEV_SERVER_URL as string}test`);
+    v.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL as string}test`);
     v.webContents.openDevTools();
+    this.listWindow.push(v);
+    console.log(this.listWindow);
     v.on('ready-to-show', () => {
       console.log('show!');
-      remote.ipcRenderer.send('Image', 'test');
+      // remote.ipcRenderer.send('Image', 'test');
       // console.log(ipcRenderer);
       // ipcRenderer.send('Image', 'test');
-      // v.show();
+      v.show();
     });
   }
   asdf2() {
@@ -47,6 +54,15 @@ export default class Preload {
   }
   asdf4() {
     console.log('asdfasdf');
+  }
+
+  GetData() {
+    Log.info('get data'); //testwindow의 preload랑 다른 object!
+    if (this.listWindow.length === 0) return;
+    const win = this.listWindow[0];
+    Log.info(this.listWindow[0]);
+    Log.info(win.title);
+    this.listWindow.splice(0, 1);
   }
 }
 
