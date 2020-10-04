3e61e86a52d525c8fa0d2babeaabee27c46e8570
hanalen <jinsub2001@naver.com>
2020-10-04 22:40:48 +0900

    트윗 데이터 class로 변경

--- Dalsae/src/Interfaces/Tweet/Tweet.ts
+++ Dalsae/src/Interfaces/Tweet/Tweet.ts
@@ -1,19 +1,43 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import * as I from '@/Interfaces';
 
-export interface Tweet {
+export class Tweet {
   created_at: string;
   id_str: string;
   full_text: string;
   entities: I.Entitie;
   extended_entities: I.ExtendedEntitie;
-  in_reply_to_status_id_str: string;
-  in_reply_to_user_id_str: string;
-  in_reply_to_screen_name: string;
-  user: I.User;
-  place: string;
+  in_reply_to_status_id_str!: string;
+  in_reply_to_user_id_str!: string;
+  in_reply_to_screen_name!: string;
+  user!: I.User;
+  place!: string;
   is_quote_status: boolean;
-  retweet_count: number;
-  favorite_count: number;
+  retweet_count!: number;
+  favorite_count!: number;
   favorited: boolean;
   retweeted: boolean;
+
+  constructor() {
+    this.created_at = '';
+    this.id_str = '';
+    this.full_text = '';
+    this.is_quote_status = false;
+    this.favorited = false;
+    this.retweeted = false;
+    this.entities = {
+      hashtags: [],
+      urls: [],
+      user_mentions: [],
+      media: {
+        id_str: '',
+        media_url_https: '',
+        url: '',
+        display_url: '',
+        expanded_url: '',
+        type: ''
+      }
+    };
+    this.extended_entities = { media: [] };
+  }
 }
