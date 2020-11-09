94677584aeae3329643971f2f087c235809cfda7
hanalen <jinsub2001@naver.com>
2020-11-09 23:20:52 +0900

    트윗 ui 관련 파일 추가

---
+++ Dalsae/src/components/Home/TweetPanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tweet-panel">
+    big-pie
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script lang="ts">
+import { DalsaeApp, DalsaePage } from '@/mixins';
+import { Vue, Mixins, Component, Ref, Provide } from 'vue-property-decorator';
+
+@Component
+export default class TweetPanel extends Mixins(DalsaePage) {
+  async created() {
+    console.log('aaaaa');
+  }
+}
+</script>
---
+++ Dalsae/src/components/Home/TweetSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tweet-selector">
+    big-pie
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script lang="ts">
+import { DalsaeApp, DalsaePage } from '@/mixins';
+import { Vue, Mixins, Component, Ref, Provide } from 'vue-property-decorator';
+
+@Component
+export default class TweetSelector extends Mixins(DalsaePage) {
+  async created() {
+    console.log('aaaaa');
+  }
+}
+</script>
