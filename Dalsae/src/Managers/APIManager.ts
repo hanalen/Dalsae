6d799d574fccdb480df92fe49017da84c402a0fa
hanalen <jinsub2001@naver.com>
2020-11-13 01:09:21 +0900

    api콜 매니저에 메시지, 에러처리 추가

--- Dalsae/src/Managers/APIManager.ts
+++ Dalsae/src/Managers/APIManager.ts
@@ -7,6 +7,9 @@ export class APIManager {
   mngTweet = new M.TweetDataManager();
   mngAccount = new M.AccountManager();
 
+  ShowMessage!: (msg: string) => void;
+  ShowConfirm!: (msg: string) => Promise<boolean>;
+
   get call() {
     return {
       statuses: {
@@ -15,10 +18,11 @@ export class APIManager {
         }
       },
       oauth: {
-        ReqToken: async (data: P.ReqToken) =>
+        ReqToken: async (data: P.ReqToken) => {
           this.api.requestOAuth<P.ReqToken>('https://api.twitter.com/oauth/request_token', {
             data
-          }),
+          });
+        },
         ReqAccessToken: async (data: P.ReqAccessToken) =>
           this.api.requestOAuth<P.ReqAccessToken>('https://api.twitter.com/oauth/access_token', {
             data
--- Dalsae/src/mixins/DalsaeApp.ts
+++ Dalsae/src/mixins/DalsaeApp.ts
@@ -33,16 +33,22 @@ export class DalsaeApp extends Vue implements MIX.DalsaePageBase {
   }
 
   async created() {
+    this.Init();
     this.LoadConfig();
     this.$nextTick(() => {
       this.StardDalsae();
     });
   }
 
-  LoadConfig() {
+  Init() {
     this.api.api.mngAccount = this.mngAccount; //나중에 이 구조는 바꾸는 게 좋을 거 같다
     this.api.mngTweet = this.mngTweet;
     this.tweetPanel.mngTweet = this.mngTweet;
+    this.api.ShowMessage = this.ShowMessage;
+    this.api.ShowConfirm = this.ShowConfirm;
+  }
+
+  LoadConfig() {
     window.preload.LoadConfig();
     this.mngAccount.switter = window.preload.LoadSwitter();
     const option = window.preload.LoadOption();
