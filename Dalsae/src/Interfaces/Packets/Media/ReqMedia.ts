6e79626442873ad6702802dc5eae2a43cd9164f2
hanalen <jinsub2001@naver.com>
2021-03-21 00:37:22 +0900

    미디어 패킷 변수 추가

--- Dalsae/src/Interfaces/Packets/Media/ReqMedia.ts
+++ Dalsae/src/Interfaces/Packets/Media/ReqMedia.ts
@@ -1,4 +1,10 @@
 export interface ReqMedia {
-  media: string;
+  media?: string;
   media_category?: string;
+  media_type?: string;
+  total_bytes?: number;
+  command?: string;
+  media_id?: string;
+  media_data?: string;
+  segment_index?: number;
 }
