6e595347505f9ad007643a2db990869a84136a1f
hanalen <jinsub2001@naver.com>
2021-04-03 23:59:52 +0900

    단어 뮤트 설정 추가

--- Dalsae/src/mixins/Modals/OptionDetailModalBase.ts
+++ Dalsae/src/mixins/Modals/OptionDetailModalBase.ts
@@ -5,16 +5,53 @@ import * as M from '@/Managers';
 
 class State {
   isShow: boolean;
-  pin: string;
+  selectMenu: number;
+  listMenu: ModalMenu[];
+  selectWord: string;
+  input: string;
   constructor() {
     this.isShow = false;
-    this.pin = '';
+    this.selectMenu = 0;
+    this.listMenu = [];
+    this.selectWord = '';
+    this.input = '';
+    const menu1: ModalMenu = {
+      title: '필터링 설정',
+      subTitle: '트윗을 필터링 해줍니다',
+      menuSub: []
+    };
+    menu1.menuSub.push({ title: '단어 뮤트', icon: 'mdi-filter', menuNumber: 0 });
+    menu1.menuSub.push({ title: '사용자 뮤트', icon: 'mdi-account-off', menuNumber: 1 });
+    menu1.menuSub.push({ title: '클라이언트 뮤트', icon: 'mdi-filter', menuNumber: 2 });
+    menu1.menuSub.push({ title: '트윗 뮤트', icon: 'mdi-filter', menuNumber: 3 });
+    this.listMenu.push(menu1);
+    const menu2: ModalMenu = {
+      title: '단축키 설정',
+      subTitle: '프로그램을 보다 편하게 ',
+      menuSub: []
+    };
+    menu2.menuSub.push({ title: '단축키 설정', icon: 'mdi-filter', menuNumber: 4 });
+    this.listMenu.push(menu2);
   }
+}
+
+interface ModalMenu {
+  title: string;
+  subTitle: string;
+  icon?: string;
+  menuSub: ModalMenuSub[];
+}
+
+interface ModalMenuSub {
+  title: string;
+  icon: string;
+  menuNumber: number;
 }
 
 @Component
 export class OptionDetailModalBase extends mixins(Vue, DalsaePage) {
   state = new State();
+  muteOption = this.mngOption.muteOption;
 
   async ShowModal() {
     this.state.isShow = true;
@@ -26,10 +63,22 @@ export class OptionDetailModalBase extends mixins(Vue, DalsaePage) {
 
   async ModalClose() {
     this.state.isShow = false;
-    this.state.pin = '';
   }
 
   CloseModal() {
     this.state.isShow = false;
+  }
+
+  OnAdd(list: string[], word: string) {
+    this.state.input = '';
+    if (list.indexOf(word) === -1)
+      //중복 등록 안되게
+      list.push(word);
+  }
+
+  OnRemove(list: string[], word: string) {
+    const index = list.indexOf(word);
+    if (index === -1) return;
+    list.splice(index, 1);
   }
 }
--- Dalsae/src/views/Modals/OptionDetailModal.vue
+++ Dalsae/src/views/Modals/OptionDetailModal.vue
@@ -1,17 +1,107 @@
 <template>
   <div class="option-detail-modal">
-    <v-dialog class="modals" ref="modal" v-model="state.isShow" max-width="300">
-      <!--전체화면-->
-      <v-card width="600px" height="800px">
-        <!--왼쪽 네비 바-->
-        <!--오른쪽은 각 메뉴별로 구성, 하위 메뉴는 탭으로 할까 했지만 디코처럼 아래로 나열하자-->
-        <!--트윗 뮤트 같은 경우는 트윗 정보 다 저장해서 해당 트윗을 표시해서 삭제하게-->
+    <!--전체화면-->
+    <v-dialog
+      class="modals"
+      ref="modal"
+      v-model="state.isShow"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <!--왼쪽 네비 바-->
+      <v-card class="flex">
+        <div class="left">
+          <v-list nav v-for="(menu, i) in state.listMenu" :key="i">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="title">
+                  {{ menu.title }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ menu.subTitle }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link v-for="(sub, j) in menu.menuSub" :key="j">
+              <v-list-item-icon>
+                <v-icon>{{ sub.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ sub.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </div>
+        <div class="center">
+          <!--탭으로 해서 표시하자-->
+          <v-tabs v-model="state.selectMenu" hide-slider hidden> </v-tabs>
+          <v-tabs-items v-model="tweetPanel.state.selectMenu">
+            <v-tab-item :key="0">
+              <v-card outlined height="100vh">
+                <v-card-title>
+                  단어 뮤트
+                </v-card-title>
+                <v-card-subtitle>
+                  특정 단어가 들어간 트윗을 필터링합니다.
+                </v-card-subtitle>
+                <v-list class="overflow-y-auto" dense max-height="300px">
+                  <v-list-item-group v-model="state.selectWord" color="primary">
+                    <v-list-item v-for="(item, i) in muteOption.keyword" :key="i">
+                      <v-list-item-content @click="state.input = item">
+                        <v-list-item-title v-text="item"></v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+                <v-card-actions>
+                  <v-text-field label="단어 입력" v-model="state.input"></v-text-field>
+                  <v-btn
+                    outlined
+                    color="primary"
+                    text
+                    @click="OnAdd(muteOption.keyword, state.input)"
+                  >
+                    추가
+                  </v-btn>
+                  <v-btn
+                    outlined
+                    color="red"
+                    text
+                    @click="OnRemove(muteOption.keyword, state.input)"
+                  >
+                    삭제
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </div>
+        <div class="right">wererawer</div>
       </v-card>
+      <!--오른쪽은 각 메뉴별로 구성, 하위 메뉴는 탭으로 할까 했지만 디코처럼 아래로 나열하자-->
+      <!--트윗 뮤트 같은 경우는 트윗 정보 다 저장해서 해당 트윗을 표시해서 삭제하게-->
     </v-dialog>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+}
+.left {
+  width: 220px;
+  background-color: aliceblue;
+}
+.center {
+  width: calc(100vw - 370px);
+  background-color: antiquewhite;
+}
+.right {
+  width: 150px;
+  background-color: gray;
+}
+</style>
 
 <script lang="ts">
 import { Mixins, Component, Ref } from 'vue-property-decorator';
