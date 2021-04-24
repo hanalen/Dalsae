5b026cf2d31d1eab423489ca141544199dc847af
hanalen <jinsub2001@naver.com>
2021-04-24 19:38:17 +0900

    스토어 트윗스토어 추가

--- Dalsae/src/store/index.ts
+++ Dalsae/src/store/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { ISwitterState } from './modules/SwitterStore';
+import { ITweetStore } from './modules/TweetStore';
 import Vuex from 'vuex';
 import Vue from 'vue';
 Vue.use(Vuex);
 
 export interface IRootState {
   switter: ISwitterState;
+  tweet: ITweetStore;
 }
 
 // Declare empty store first, dynamically register all modules later.
