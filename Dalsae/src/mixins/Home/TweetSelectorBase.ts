a278aefa279eed736c32689b5c41772655f64b88
hanalen <jinsub2001@naver.com>
2021-10-16 20:53:57 +0900

    Context 열려있는 상태에서 트윗 클릭  비정상 동작 수정

--- Dalsae/src/mixins/Home/TweetSelectorBase.ts
+++ Dalsae/src/mixins/Home/TweetSelectorBase.ts
@@ -49,7 +49,7 @@ export class TweetSelectorBase extends Vue {
     return moduleUI.stateContext.isShow && moduleUI.stateContext.tweet.id_str === this.tweet.id_str;
   }
   set isShowContext(isShow: boolean) {
-    eventBus.$emit('OnClickTweet', this.tweet.id_str);
+    if (isShow) eventBus.$emit('OnClickTweet', this.tweet.id_str);
     const { x, y } = moduleUI.stateContext;
     moduleUI.OnContext({ x: x, y: y, isShow: isShow, maxIndex: 0, listContext: this.listContext });
   }
