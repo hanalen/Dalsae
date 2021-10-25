15df10e05b4cd2babe6b99a98a60467316df1f02
hanalen <jinsub2001@naver.com>
2021-10-26 03:32:07 +0900

    body 기본 css설정
    스타일 지정이 안 먹혀서 코드로 강제 적용

--- Dalsae/src/App.vue
+++ Dalsae/src/App.vue
@@ -9,5 +9,17 @@ import { DalsaeApp } from '@/mixins';
 import { Vue, Mixins, Component, Ref, Provide } from 'vue-property-decorator';
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  async created() {
+    this.$nextTick(() => {
+      const html = document.getElementsByTagName('html');
+      html[0].style.setProperty('overflow-y', 'hidden');
+      html[0].style.setProperty('-ms-user-select', 'none');
+      html[0].style.setProperty('-moz-user-select', '-moz-none');
+      html[0].style.setProperty('-webkit-user-select', 'none');
+      html[0].style.setProperty('user-select', 'none');
+      html[0].style.setProperty('-webkit-font-smoothing', 'antialiased');
+    });
+  }
+}
 </script>
