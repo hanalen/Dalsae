742f52afb7d4621e07098a9d9e6d9f529f1ba1d6
hanalen <jinsub2001@naver.com>
2021-10-26 23:12:10 +0900

    이미지 뷰어 트윗 크기 제한 추가

--- Dalsae/src/views/ImageView.vue
+++ Dalsae/src/views/ImageView.vue
@@ -2,8 +2,23 @@
   <!-- v-app으로 변경 v-app으로 바꾸면 트윗,이미지,보톰 사이즈 알아서 계산 해줌 -->
   <v-app>
     <v-main app>
-      <v-container fluid>
-        <tweet-image v-if="option.isShowTweet" :tweet="tweet" :option="option"></tweet-image>
+      <v-container fluid :style="styleTop">
+        <div :style="styleTweet" ref="refTweet">
+          <tweet-image v-if="option.isShowTweet" :tweet="tweet" :option="option"></tweet-image>
+        </div>
+        <v-btn
+          v-if="isBigTweet && !bExpanded"
+          icon
+          rounded
+          width="100vw"
+          height="30"
+          class="btn-expand"
+          @click="OnClickExpand"
+        >
+          <v-icon :color="'primary'" size="40">
+            mdi-chevron-down
+          </v-icon>
+        </v-btn>
       </v-container>
       <v-container :id="main">
         <image-content :tweet="tweet" :index="index"> </image-content>
@@ -22,6 +37,9 @@
 #main {
   height: calc(100% - 120px) !important;
 }
+.btn-expand {
+  background-color: rgba(160, 160, 160, 0.11);
+}
 .bottom {
   display: flex;
 }
@@ -31,12 +49,42 @@
 </style>
 
 <script lang="ts">
-import { Vue, Component, Mixins } from 'vue-property-decorator';
+import { Vue, Component, Mixins, Ref } from 'vue-property-decorator';
 import * as I from '@/Interfaces';
 import * as MIX from '@/mixins';
-
 @Component
 export default class ImageView extends Mixins(MIX.ImagePage) {
+  @Ref()
+  refTweet!: HTMLElement;
+  bExpanded = false;
+  bMounted = false;
+  get styleTop() {
+    if (!this.bExpanded) {
+      return {
+        'max-height': '180px'
+      };
+    }
+  }
+  get styleTweet() {
+    if (!this.bExpanded) {
+      return {
+        'max-height': '150px',
+        overflow: 'hidden'
+      };
+    }
+  }
+  get isBigTweet() {
+    if (!this.bMounted) return;
+    const height = this.refTweet.clientHeight;
+    if (height >= 150) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  OnClickExpand(e: MouseEvent) {
+    this.bExpanded = true;
+  }
   ClickLink(e: Event) {
     // const listTweet: I.Tweet[] = TweetDataManager.listTweet as I.Tweet[];
     // console.log(listTweet[this.index]);
@@ -52,6 +100,9 @@ export default class ImageView extends Mixins(MIX.ImagePage) {
   }
 
   async created() {
+    this.tweet = window.preload.LoadTestImageTweet();
+    this.$nextTick(() => {
+      this.bMounted = true;
     console.log('img window created');
     const id = this.$route.query.tweetId;
     console.log('id: ' + id);
