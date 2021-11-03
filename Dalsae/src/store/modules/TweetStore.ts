3048290f6ac8039147d634ef61033f0a71388d43
hanalen <jinsub2001@naver.com>
2021-11-04 01:39:42 +0900

    TweetStore Tweet추가 로직 전체 추가

--- Dalsae/src/store/modules/TweetStore.ts
+++ Dalsae/src/store/modules/TweetStore.ts
@@ -128,15 +128,76 @@ class TweetStore extends VuexModule {
 
   @Mutation
   private addFavorite(addTweet: A.AddTweet) {
-    console.log(addTweet);
+    let tweets = this.stateTweet.tweets.find(x => x.key === moduleSwitter.selectID);
+    if (!tweets) {
+      tweets = { key: moduleSwitter.selectID, tweets: new I.Tweets() };
+      this.stateTweet.tweets.push(tweets);
+    }
+    const { tweet, listTweet, user_id_str } = addTweet;
+    const { dicMuteIds, listBlockIds } = moduleSwitter.stateIds;
+    const { muteOption } = moduleOption;
+    const listMuteIds = dicMuteIds.get(moduleSwitter.selectID);
+
+    const orgTweets = tweets.tweets.favorites;
+    let listConcatTweets: I.Tweet[] = [];
+    listConcatTweets = tweet ? [tweet] : [];
+
+    if (listTweet) {
+      listConcatTweets = listConcatTweets.concat(listTweet);
+    }
+    if (tweet) {
+      listConcatTweets = [tweet];
+    }
+    listConcatTweets.forEach(item => {
+      if (orgTweets.find(x => x.id_str === addTweet.tweet?.id_str)) {
+        console.log('exists');
+        return; //exists
+      }
+      if (!CheckShowMentionTweet(item, user_id_str, muteOption, listBlockIds, listMuteIds)) return; //muted
+      const idx = FindTweetIndex(item, orgTweets);
+      orgTweets.splice(idx, 0, new I.Tweet(item));
+    });
   }
   @Mutation
   private addOpen(addTweet: A.AddTweet) {
-    console.log(addTweet);
+    let tweets = this.stateTweet.tweets.find(x => x.key === moduleSwitter.selectID);
+    if (!tweets) {
+      tweets = { key: moduleSwitter.selectID, tweets: new I.Tweets() };
+      this.stateTweet.tweets.push(tweets);
+    }
+    const { tweet } = addTweet;
+    if (!tweet) return;
+    const orgTweets = tweets.tweets.opens;
+
+    if (orgTweets.find(x => x.id_str === addTweet.tweet?.id_str)) {
+      console.log('exists');
+      return; //exists
+    }
+    const idx = FindTweetIndex(tweet, orgTweets);
+    orgTweets.splice(idx, 0, new I.Tweet(tweet));
   }
   @Mutation
   private addConv(addTweet: A.AddTweet) {
-    console.log(addTweet);
+    let tweets = this.stateTweet.tweets.find(x => x.key === moduleSwitter.selectID);
+    if (!tweets) {
+      tweets = { key: moduleSwitter.selectID, tweets: new I.Tweets() };
+      this.stateTweet.tweets.push(tweets);
+    }
+    const { tweet, user_id_str } = addTweet;
+    const { dicMuteIds, listBlockIds } = moduleSwitter.stateIds;
+    const { muteOption } = moduleOption;
+    const listMuteIds = dicMuteIds.get(moduleSwitter.selectID);
+    if (!tweet) return;
+    const orgTweets = tweets.tweets.conv;
+
+    if (orgTweets.find(x => x.id_str === addTweet.tweet?.id_str)) {
+      console.log('exists');
+      return; //exists
+    }
+    if (!CheckShowMentionTweet(tweet, user_id_str, muteOption, listBlockIds, listMuteIds)) return; //muted
+
+    const idx = FindTweetIndex(tweet, orgTweets);
+    orgTweets.splice(idx, 0, new I.Tweet(tweet));
   }
 
   @Action
@@ -208,8 +269,10 @@ class TweetStore extends VuexModule {
   @Mutation
   private clearConv(user_id_str: string) {
     const find = this.stateTweet.tweets.find(x => x.key === user_id_str);
+    console.log('clreaconv', find);
     if (find) {
-      find.tweets.conv = [];
+      find.tweets.conv.splice(0, 999);
+      console.log(find.tweets.conv);
     }
   }
 
