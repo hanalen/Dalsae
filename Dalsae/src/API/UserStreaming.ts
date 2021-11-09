86bb8921559ca07078947b9a0c2f3cadd7156c4b
hanalen <jinsub2001@naver.com>
2021-11-09 18:06:04 +0900

    스트리밍 private처리 및 연결 상태에 따라 시스템바 추가

--- Dalsae/src/API/UserStreaming.ts
+++ Dalsae/src/API/UserStreaming.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import * as I from '@/Interfaces';
-import { ETweetType } from '@/store/Interface';
+import { ESystemBar, ETweetType } from '@/store/Interface';
+import { moduleSysbar } from '@/store/modules/SystemBarStore';
 import { moduleTweet } from '@/store/modules/TweetStore';
 import axios from 'axios';
 import { CreateHeader } from './TwitterRequest';
 export class UserStreaming {
-  idStr = '';
-  reader!: ReadableStreamDefaultReader<Uint8Array>;
-  decoder = new TextDecoder();
-  json = '';
-  controller!: AbortController;
-  signal!: AbortSignal;
+  private idStr = '';
+  private reader!: ReadableStreamDefaultReader<Uint8Array>;
+  private decoder = new TextDecoder();
+  private json = '';
+  private controller!: AbortController;
+  private signal!: AbortSignal;
   async Connect(publicKey: string, secretKey: string, idStr: string) {
     this.idStr = idStr;
     this.controller = new AbortController();
@@ -37,27 +38,44 @@ export class UserStreaming {
       });
   }
   StopStreaming() {
-    console.log('stop streaming!!!');
+    console.log('stop streaming!!!  id: ', this.idStr);
     this.controller.abort();
+    moduleSysbar.RemoveSystemBar(ESystemBar.EStreaming);
+    moduleSysbar.AddSystemBar({
+      type: ESystemBar.EErrorStreaming,
+      icon: 'mdi-sync-off',
+      text: '',
+      toolTip: '유저 스트리밍 호흡기 연결 해제 됨'
+    });
   }
-  SteamingResponse(response: Response) {
+  private SteamingResponse(response: Response) {
     if (!response.body) return;
     this.reader = response.body.getReader();
+    if (this.reader) {
+      moduleSysbar.RemoveSystemBar(ESystemBar.EErrorStreaming);
+      moduleSysbar.AddSystemBar({
+        type: ESystemBar.EStreaming,
+        icon: 'mdi-sync',
+        text: '',
+        toolTip: '유저 스트리밍 호흡기 연결 됨'
+      });
+    }
     return this.ReadStreaming();
   }
-  StreamingError(err: Error) {
+  private StreamingError(err: Error) {
     console.log('Streaming Error');
     console.error(err);
+    this.StopStreaming();
     // setTimeout(() => {
     //   this.StartStreaming();
     // }, 3000);
   }
-  async ReadStreaming() {
+  private async ReadStreaming() {
     console.log('read streaming...');
     const result = await this.reader.read();
     this.AppendJson(result);
   }
-  AppendJson(result: ReadableStreamDefaultReadResult<any>) {
+  private AppendJson(result: ReadableStreamDefaultReadResult<any>) {
     const chunk = this.decoder.decode(result.value || new Uint8Array(), { stream: !result.done });
     if (chunk !== '\r\n') {
       //keep-alive 패킷이 아닌 게 들어 왔을 때에만 동작
@@ -77,7 +95,7 @@ export class UserStreaming {
       return this.ReadStreaming();
     }
   }
-  ParseJson(json: string) {
+  private ParseJson(json: string) {
     if (json.length < 10) return; //이상 패킷으로 예상 됨
 
     try {
