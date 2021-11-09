05b4822c6d38b9e577daa0e6cc73fd3eade6f175
hanalen <jinsub2001@naver.com>
2021-11-10 02:12:37 +0900

    dm인터페이스 수정

--- Dalsae/src/Interfaces/DirectMessage/DMEvent.ts
+++ Dalsae/src/Interfaces/DirectMessage/DMEvent.ts
@@ -3,6 +3,6 @@ import * as I from '@/Interfaces';
 export interface DMEvent {
   type: string;
   id?: string;
-  created_timestamp?: string;
+  created_timestamp: string;
   message_create?: I.MessageCreate;
 }
