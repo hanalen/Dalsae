c0836f1a868025741f42aa5e9db4dcc772ce7244
hanalen <jinsub2001@naver.com>
2021-11-09 22:09:21 +0900

    DomStore 추가

---
+++ Dalsae/src/store/modules/DomStore.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import * as I from '@/Interfaces';
+import { Module, VuexModule, Mutation, Action, getModule } from 'vuex-module-decorators';
+import store from '@/store';
+import * as M from '@/mixins';
+
+@Module({ dynamic: true, store, name: 'dom' })
+class DomStore extends VuexModule {
+  listScrollPanel: M.ScrollPanelBase[] = [];
+
+  @Mutation
+  private registeScrollPanel(panel: M.ScrollPanelBase) {
+    this.listScrollPanel.push(panel);
+  }
+
+  @Action
+  RegisteScrollPanel(panel: M.ScrollPanelBase) {
+    this.context.commit('registeScrollPanel', panel);
+  }
+}
+
+export const moduleDom = getModule(DomStore);
