3afddc4c922dc64144f85e19b1f8348886e9ff22
hanalen <jinsub2001@naver.com>
2021-11-28 05:03:53 +0900

    Vue 센트리 글로벌 이벤트 추가

--- Dalsae/src/main.ts
+++ Dalsae/src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-// import VirtualScrollList from 'vue-virtual-scroll-list';
-// import VueVirtualScroller from 'vue-virtual-scroller';
-// import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
+import * as Sentry from '@sentry/vue';
+import { Integrations } from '@sentry/tracing';
+import { Breadcrumb } from '@sentry/vue';
+// import {Integrations} from '@sentry/node';
+
 function register() {
   const files = require.context('./', true, /\.vue$/i);
   files
@@ -13,13 +15,42 @@ function register() {
     .map(key => Vue.component((key.split('/').pop() || '').split('.')[0], files(key).default));
 }
 
-// Vue.component('virtual-scroll', VirtualScrollList);
-// Vue.use(VueVirtualScroller);
 // Vue 컴포넌트를 자동으로 등록
 register();
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+Sentry.init({
+  Vue,
+  maxBreadcrumbs: 4,
+  dsn: 'https://98af9300813e4906ab16aa0a336be0bd@o146584.ingest.sentry.io/6083906',
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['localhost', 'my-site-url.com', /^\//],
+      traceXHR: false
+    })
+  ],
+  beforeBreadcrumb(breadcrumb, hint) {
+    if (breadcrumb.category !== 'console') return null;
+    else {
+      return breadcrumb;
+    }
+  },
+  beforeSend: (e: Sentry.Event, hint: Sentry.EventHint) => {
+    if (e.user) {
+      delete e.user;
+    }
+    if (e.platform) {
+      delete e.platform;
+    }
+    return e;
+  },
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+});
 
 new Vue({
   router,
